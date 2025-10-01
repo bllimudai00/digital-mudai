b62c65df1f548765caa8b57ecad9fc3adb7030d9
Digital Mudai <digitalmudai@gmail.com>
2025-10-01 04:05:44 +0000

    Slov it

--- src/context/TonConnectProvider.tsx
+++ src/context/TonConnectProvider.tsx
@@ -6,7 +6,7 @@ import { ReactNode } from "react";
 
 // The manifest is used to configure the TonConnect UI.
 // It includes metadata about your app that will be displayed to the user in the connection modal.
-const manifestUrl = "https://raw.githubusercontent.com/Pari-Network/parinetwork/main/public/tonconnect-manifest.json";
+const manifestUrl = "https://gist.githubusercontent.com/barbarbar28/1d60721e33cb4115456d14166874838b/raw/c34762514115a1f6305a5078564882b53580575d/tonconnect-manifest.json";
 
 export function TonConnectProvider({ children }: { children: ReactNode }) {
     return (
